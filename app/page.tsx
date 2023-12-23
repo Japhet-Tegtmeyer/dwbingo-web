1267bac1ec6fb2c86cb3f8a6822e766e75240f8d
Japhet Tegtmeyer <139924818+Japhet-Tegtmeyer@users.noreply.github.com>
2023-12-23 15:20:26 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ const Home: React.FC = () => {
     <main>
       <Image
       className='rounded-[35px] flex justify-center m-auto mb-7 mt-32'
-      src="/newico.png"
+      src="/dwbingoappicon.png"
       width={150}
       height={150}
       alt="Picture of the author"
     />
         <h1 className='flex justify-center font-bold text-xl'>DWBingo for iOS</h1>
         <h2 className='flex justify-center font-black text-4xl tracking-tight text-center p-5'>A natively built <br /> bingo game. Open <br />  source. Free  <br /> Forever.</h2>
-        <h3 className='flex justify-center font-medium text-gray-700 text-center pl-[30px] pr-[30px] mt-5'>DWBingo is an exciting new bingo game written entirely and unapologetically for iOS. With haptics, over 15 bingo cards and a key focus on privacy it's an app that feels right at home on your iPhone  </h3>
-        <a href=""><h4 className='flex justify-center p-3 ml-16 mr-16 rounded-full bg-blue-600 m-7 mt-10'>Download Alpha</h4></a>
+        <h3 className='flex justify-center font-medium text-gray-700 text-center pl-[30px] pr-[30px] mt-5'>DWBingo is an exciting new bingo game written entirely and unapologetically for iOS. Over 15 bingo cards and 40 unique squares.</h3>
+        <a href="https://apps.apple.com/us/app/dwbingo/id6472356354"><h4 className='flex justify-center p-3 ml-16 mr-16 rounded-full bg-blue-600 m-7 mt-10'>Download Alpha</h4></a>
         <div className='justify-center bg-slate-800 rounded-xl ml-5 mr-5 mt-10'>
           <h1 className='p-[1px]'></h1>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 m-5">
